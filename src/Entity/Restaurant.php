e970f9930b2a76d89956452c6b2277ae7bed5ea1
Popeye <bastien.bourgeat1@gmail.com>
2023-02-26 19:17:19 +0100

    project initialization

---
+++ src/Entity/Restaurant.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RestaurantRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RestaurantRepository::class)]
+class Restaurant
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $seatingCapacity = null;
+
+    #[ORM\Column]
+    private ?int $seatsFilled = null;
+
+    #[ORM\OneToMany(mappedBy: 'restaurant', targetEntity: Booking::class, orphanRemoval: true)]
+    private Collection $booking;
+
+    public function __construct()
+    {
+        $this->booking = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSeatingCapacity(): ?int
+    {
+        return $this->seatingCapacity;
+    }
+
+    public function setSeatingCapacity(int $seatingCapacity): self
+    {
+        $this->seatingCapacity = $seatingCapacity;
+
+        return $this;
+    }
+
+    public function getSeatsFilled(): ?int
+    {
+        return $this->seatsFilled;
+    }
+
+    public function setSeatsFilled(int $seatsFilled): self
+    {
+        $this->seatsFilled = $seatsFilled;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Booking>
+     */
+    public function getBooking(): Collection
+    {
+        return $this->booking;
+    }
+
+    public function addBooking(Booking $booking): self
+    {
+        if (!$this->booking->contains($booking)) {
+            $this->booking->add($booking);
+            $booking->setRestaurant($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBooking(Booking $booking): self
+    {
+        if ($this->booking->removeElement($booking)) {
+            // set the owning side to null (unless already changed)
+            if ($booking->getRestaurant() === $this) {
+                $booking->setRestaurant(null);
+            }
+        }
+
+        return $this;
+    }
+}
