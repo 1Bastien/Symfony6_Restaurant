fe584f06ea2d396065e4ea2de4e29e27c8024d7a
Popeye <bastien.bourgeat1@gmail.com>
2023-03-13 21:50:36 +0100

    form input type

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Customer;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,7 +16,7 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', null, ['label' => 'Mail', 'constraints' => [
+            ->add('email', EmailType::class, ['label' => 'Mail', 'constraints' => [
                 new NotBlank([
                     'message' => "Merci d'entrer votre email.",
                 ])
